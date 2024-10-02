e199672c0cce346e946373f89a35dc90c470ac1f
rayye <rim.abdennour02@gmail.com>
2024-10-02 09:06:09 +0200

    Added mockito tool.

---
+++ src/test/com/tves/CarTestMockito.java
@@ -0,0 +1,13 @@
+package com.tves;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockMakers;
+
+class CarTestMockito {
+    @Mock
+
+    @Test
+    public void testMockitoConstructor() {
+ }
+}
